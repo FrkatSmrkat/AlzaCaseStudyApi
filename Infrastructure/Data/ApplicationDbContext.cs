f693cb1bd1240f6b68e71bd3f935817cf0e0380b
Frantisek Cech <frantisek.cech@czech-kiosk.cz>
2021-11-24 22:33:04 +0100

    Infrastructure basic WIP

---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().ToTable("Product");
+        }
+    }
+}
